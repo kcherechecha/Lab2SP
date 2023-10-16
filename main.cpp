b6b3731f22f3c32a28eccc837da53267f1437c79
kcherechecha <115093902+kcherechecha@users.noreply.github.com>
2023-10-16 10:46:45 +0300

    search words added

--- main.cpp
+++ main.cpp
@@ -39,11 +39,8 @@ Automaton readAutomatonFile(const std::string &fileName) {
     return automaton;
 }
 
-std::vector<int> computePrefixFunction(const char* pattern) {
-    int m = 0;
-    while (pattern[m] != '\0') {
-        ++m;
-    }
+std::vector<int> computePrefixFunction(const std::string& pattern) {
+    int m = pattern.size();
 
     std::vector<int> prefixFunction(m, 0);
     int k = 0;
@@ -59,11 +56,8 @@ std::vector<int> computePrefixFunction(const char* pattern) {
     return prefixFunction;
 }
 
-bool hasPeriodicFragment(const char* word) {
-    int n = 0;
-    while (word[n] != '\0') {
-        ++n;
-    }
+bool hasPeriodicFragment(const std::string& word) {
+    int n = word.size();
 
     std::vector<int> prefixFunction = computePrefixFunction(word);
 
@@ -72,17 +66,17 @@ bool hasPeriodicFragment(const char* word) {
     return lastPrefix > 0 && n % (n - lastPrefix) == 0;
 }
 
-bool acceptWord(Automaton fa, const char word[]) {
-    int currentState = fa.startState;
-    for (const char* ptr = word; *ptr != '\0'; ++ptr) {
-        char ch = *ptr;
 
+bool acceptWord(Automaton fa, std::string word) {
+    int currentState = fa.startState;
+    for (const char& ch : word) {
         if (fa.transitions.at(currentState).find(ch) == fa.transitions.at(currentState).end()) {
             return false;
         }
 
         currentState = fa.transitions.at(currentState).at(ch);
     }
+
 
     for (int state : fa.acceptingStates) {
         if (currentState == state)
@@ -93,31 +87,39 @@ bool acceptWord(Automaton fa, const char word[]) {
     return false;
 }
 
+std::vector<std::string> generateWords(const Automaton &automaton) {
+    std::vector<std::string> acceptedWords;
+
+    for (int len = 1; len <= 7; ++len) {
+        for (int i = 0; i < (1 << len); ++i) {
+            std::string word;
+            for (int j = 0; j < len; ++j) {
+                if (i & (1 << j)) word += 'b';
+                else word += 'a';
+            }
+            if (acceptWord(automaton, word)) {
+                acceptedWords.push_back(word);
+            }
+        }
+    }
+
+    return acceptedWords;
+}
+
+
+
 int main() {
     std::vector<std::string> acceptedWords;
 
-    Automaton automaton = readAutomatonFile("/Users/kcherechecha/CLionProjects/untitled25/automata3.txt");
+    Automaton automaton = readAutomatonFile("../automata3.txt");
 
-    std::ifstream inputFile("/Users/kcherechecha/CLionProjects/untitled25/words3.txt");
 
-    if (!inputFile.is_open() || inputFile.peek() == std::ifstream::traits_type::eof()) {
-        std::cerr << "Не вдалося відкрити файл зі словами\n";
-        return 1;
-    }
 
-    std::vector<std::string> words;
-
-    std::string word;
-    while (inputFile >> word) {
-        words.push_back(word);
-    }
+    std::vector<std::string> words = generateWords(automaton);
 
     std::cout << "Слова без періодичних фрагментів, які приймає автомат: " << std::endl;
     for (const auto& w : words) {
-        const char* word = w.c_str();
-        if (acceptWord(automaton, word)) {
-            std::cout << word << std::endl;
-        }
+        std::cout << "Слово " + w + " приймається автоматом\n";
     }
 
     return 0;
